695ffe9e0cee3b864789bfa37c83ce8ed371e08b
MihailTanasoglo <heterszeus@gmail.com>
2022-11-18 13:51:09 +0200

    Add project files.

---
+++ Domain/Models/Command.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Constants;
+
+namespace Domain.Models
+{
+    public class Command
+    {
+        public Guid CommandId { get; set; }
+        public string Name { get; set; } = ConstantStrings.NewCommand;
+        public string? Description { get; set; } 
+        public int CommandCode { get; set; } 
+    }
+}
---
+++ IoT_Project/Controllers/RemoteController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IoT_Project.Controllers
+{
+    public class RemoteController: ControllerBase
+    {
+        private readonly int _baudRate = 9600;
+        private readonly string _portName = "COM100";
+
+        [HttpGet]
+        public IActionResult GetCommand(string command)
+        {
+
+            return Ok(command);
+        }
+        [HttpPost]
+        public IActionResult InsertCommand(string command)
+        {
+            return Ok(command);
+        }
+
+    }
+}
